c3a218158bc0e4d7060312ea6fef792c5295d136
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2021-12-21 21:20:37 +0200

    Arrow Functions. DOM Methods

--- 05_Functions/main.js
+++ 05_Functions/main.js
@@ -13,9 +13,9 @@ function ShowUserName() {
 }
 
 // Execute
-ShowUserName();
-ShowUserName();
-ShowUserName();
+// ShowUserName();
+// ShowUserName();
+// ShowUserName();
 
 // Set Function Parameter
 function ShowNumbers(max) {
@@ -30,13 +30,13 @@ function ShowNumbers(max) {
 
 let limit = 10 * 2;
 
-ShowNumbers(limit);
-ShowNumbers(15, 4, 67);
-ShowNumbers(4);
+// ShowNumbers(limit);
+// ShowNumbers(15, 4, 67);
+// ShowNumbers(4);
 
 // Set Multiple Parameters
 
-function ShowLine(length, symbol) {
+function ShowLine(length, symbol = '#') {
 
     for (let i = 0; i < length; ++i) {
         
@@ -45,13 +45,54 @@ function ShowLine(length, symbol) {
     document.write(`<br>`);
 }
 
-ShowLine(7, '@');
+ShowLine(7); // symbol = default
 ShowLine(33, '*');
 
 function ShowRandomValue(min, max) {
     console.log(Math.round(Math.random() * (max - min) + min));
 }
 
-ShowRandomValue(1, 10);
-ShowRandomValue(50, 60);
-ShowRandomValue(0, 1);
+// ShowRandomValue(1, 10);
+// ShowRandomValue(50, 60);
+// ShowRandomValue(0, 1);
+
+// -=-=-=-=-=-=- Advanced -=-=-=-=-=-=-
+// Anonymous Function
+
+(function (word) {
+    console.log("Your word is ", word);
+})("Apple");
+
+// Function Variables
+
+// let showSumma = function (a, b) {
+//     console.log(`${a} + ${b} = ${a + b}`);
+// }
+
+// Arrow Function
+
+let showSumma = (a, b) => console.log(`${a} + ${b} = ${a + b}`);
+
+// ShowSumma(4, 7);
+// ShowSumma(123, 34);
+
+const container = document.getElementById("block-container");
+
+function addBlock(size, color = "Black") {
+
+    let block = document.createElement("div");
+
+    block.setAttribute('class', 'block');
+
+    block.style.backgroundColor = color;
+    block.style.width = size + "px";
+    block.style.height = size + "px";
+
+    container.appendChild(block);
+    //container.innerHTML += `<div style='width: ${size}px; height: ${size}px; background: ${color};' class='block'></div>`;
+}
+
+addBlock(50);
+addBlock(120);
+addBlock(120, "Green");
+addBlock(100, "Blue");

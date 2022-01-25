f0d0f6c091350b276ee6b88aabd90885b812a8d9
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-01-25 19:08:52 +0200

    Inner objects

--- 10_Objects/main.js
+++ 10_Objects/main.js
@@ -47,13 +47,75 @@ function switchDoor(door) {
     door.isLock = !door.isLock;
 }
 
-showDoor(door);
-switchDoor(door);
-showDoor(door);
-switchDoor(door);
-showDoor(door);
+// testing
+// showDoor(door);
+// switchDoor(door);
+// showDoor(door);
+// switchDoor(door);
+// showDoor(door);
 
 // get all properties of the object
-for (const key in door) {
-    console.log(`Key: ${key}, Value: ${door[key]}`);
+// for (const key in door) {
+//     console.log(`Key: ${key}, Value: ${door[key]}`);
+// }
+
+// Inner Objects
+
+let pc = {
+    year: 2018,
+    model: "Dell Super II",
+    processor: {
+        model: "Intel Core i5",
+        freq: 2.5,
+        cores: 4,
+        threads: 8
+    },
+    memory: {
+        type: "DDR4",
+        capacity: 8,
+        freq: 3200
+    },
+    ssd: {
+        model: "Samsung Evo",
+        capacity: 512
+    }
+};
+
+function showPC(pc) {
+    document.write(`\tPC: ${pc.model} ${pc.year}<br>`);
+    document.write(`Processor: ${pc.processor.model}<br>`);
+    document.write(`Memory: ${pc.memory.type}<br>`);
+    document.write(`SSD: ${pc.ssd.model}<br>`);
 }
+function turboBoost(processor) {
+    console.log(`Boost the processor ${processor.model} from ${processor.freq}Ghz...`);
+    processor.freq += 0.33;
+    console.log(`Final frequency ${processor.freq}Ghz`);
+}
+
+// testing
+showPC(pc);
+turboBoost(pc.processor);
+
+let order = {
+    number: 233233,
+    date: new Date(2022, 1, 25),
+    price: 4500,
+    client: {
+        firstName: "Sasha",
+        lastName: "Programmer",
+        phoneNumber: "+380987654321",
+        address: {
+            country: "Ukraine",
+            city: "Rivne",
+            street: "Soborna",
+            building: "5A"
+        }
+    }
+};
+
+function deliverOrder(address) {
+    console.log(`Shipping the order to ${address.city} ${address.country}, ${address.street} ${address.building}`);
+}
+
+deliverOrder(order.client.address);

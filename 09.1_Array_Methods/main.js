71a161194a0ab59f3fdeacd4c50c971de0c0592b
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-02-08 19:44:44 +0200

    Working with Array Methods

--- 09.1_Array_Methods/main.js
+++ 09.1_Array_Methods/main.js
@@ -1,3 +1,5 @@
+//---------- Array Methods ----------
+
 let books = [
     { 
         title: 'Nigthmare',
@@ -26,6 +28,7 @@ let books = [
     }
 ];
 
+//---------- Sorting ----------
 function compareBooksByTitle(a, b) {
     if (a.title < b.title) return -1;
     if (a.title > b.title) return 1;
@@ -35,8 +38,85 @@ function compareBooksByTitle(a, b) {
 //     return a.rating - b.rating;
 // }
 
+// Arrow function
 books.sort((a, b) => b.rating - a.rating);
 
-for (const book of books) {
-    console.log(book);
+// for (const book of books) {
+//     console.log(book);
+// }
+console.log(books);
+
+////////// Bubble Sort
+function bubbleSort(arr) {
+
+    //Outer pass
+    for(let i = 0; i < arr.length; i++) {
+
+        //Inner pass
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            //Value comparison using ascending order
+
+            if(arr[j + 1] < arr[j]){
+
+                //Swapping
+                [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
+            }
+        }
+    };
+    return arr;
+};
+
+console.log(bubbleSort([5,3,8,4,6]));
+
+//---------- Working with Array ----------
+let numbers = [ 5, 79, 34, 4, 33, 10, 33, 50, 48 ];
+
+console.log("Element at index 3 = ", numbers[3]); // 4
+
+//---------- indexOf ----------
+console.log("Index of first 33 = ", numbers.indexOf(33)); // search from the beginning
+console.log("Index of last 33 = ", numbers.lastIndexOf(33)); // from the end
+
+// return -1 if element is not exists
+if (numbers.indexOf(2) === -1) {
+    console.warn("2 does not exists");
 }
+
+console.log(numbers.indexOf(33, 3)); // 4
+
+//---------- fill ----------
+numbers.fill(777, 5, 8); // fill elemets with 777 from index [5...8)
+
+console.log("After fill: ", numbers);
+
+//---------- find ----------
+console.log("Even number: ", numbers.find((n, i) => n % 2 === 0 && i % 2 != 0));
+
+//---------- forEach ----------
+numbers.forEach((num, ind) => {
+    console.log(`Element at index ${ind} = ${num}`);
+})
+// the same but with for
+// for (let ind = 0; ind < numbers.length; ind++) {
+//     console.log(`Element at index ${ind} = ${numbers[ind]}`);
+// }
+
+//---------- slice ----------
+let copy = numbers.slice(2, 5); 
+console.log("Slice from 2 to 5: ", copy);
+console.log("Original after slice: ", numbers);
+
+//---------- splice ----------
+numbers.splice(2, 3); // remove 3 elements from 2 index
+console.log("Original after splice: ", numbers);
+
+//---------- reverse ----------
+const reversed = [...numbers]; // copy of the numbers array
+reversed.reverse();
+console.log("Reversed: ", reversed);
+console.log("Original: ", numbers);
+
+//---------- map ----------
+const mapped = numbers.map((n) => `${n}!!!`);
+console.log("Mapped array: ", mapped);
